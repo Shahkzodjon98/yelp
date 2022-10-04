2b2bf0dc2b93821c8e0b2cbdd5024389a558c4d3
Shakhzod <105542321+Shahkzodjon98@users.noreply.github.com>
2022-10-04 15:31:00 +0500

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,8 @@
 import Navbar from 'react-bootstrap/Navbar';
 import Container from 'react-bootstrap/Container';
 import Button from 'react-bootstrap/Button';
+import "../styles/header.css";
+
 
 export default function YelpNavbar({signOut}){
     return (
@@ -10,6 +12,7 @@ export default function YelpNavbar({signOut}){
             <Navbar.Toggle />
             <Navbar.Collapse className="justify-content-end">
                 <Navbar.Text>
+                 <Button className="signout" onClick={signOut}>Sign Out</Button><br></br>
                 <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search"></input><br></br>
                 <button class="btn btn-danger" type="submit">Search</button>
                  {/* <a href="/signup?return_url=https://www.yelp.com/" class="hide-below-b__09f24__vjFMq css-4ut3yi" data-activated="false" data-button="true"><span class="css-1enow5j" data-font-weight="semibold">Sign Up</span></a>  */}
