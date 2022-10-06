4b7b896742b8491aca54bcdab21c51d937eab326
Shakhzod <105542321+Shahkzodjon98@users.noreply.github.com>
2022-10-06 16:15:03 +0500

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -12,11 +12,6 @@ export default function YelpNavbar({signOut}){
             <Navbar.Toggle />
             <Navbar.Collapse className="justify-content-end">
                 <Navbar.Text>
-                 <br></br>
-                <form class="d-flex" role="search">
-                 <input class="form-control me-2"type="search" autocomplete="off" spellcheck="false" role="combobox" placeholder="Введите поисковый запрос или URL" aria-live="polite"></input>
-                 <button class="btn btn-Info" type="submit">Search</button>
-                  </form>
                 <br></br>
                 <Button className="signout" onClick={signOut}>Sign Out</Button><br></br>
                  {/* <a href="/signup?return_url=https://www.yelp.com/" class="hide-below-b__09f24__vjFMq css-4ut3yi" data-activated="false" data-button="true"><span class="css-1enow5j" data-font-weight="semibold">Sign Up</span></a>  */}
